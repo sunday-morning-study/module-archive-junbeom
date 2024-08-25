67fd63577e219c857725d02c58b5f72c376f0674
junbeom-Son <junbeom247@gmail.com>
2024-08-25 11:07:44 +0900

    initial commit

---
+++ hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/HexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.junbeom.hexagonalarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureApplication.class, args);
+    }
+
+}
---
+++ hexagonal-architecture/src/test/java/com/junbeom/hexagonalarchitecture/HexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junbeom.hexagonalarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
