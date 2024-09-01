1c668fc760f4bb2187d2048e2ccead5d786b23cc
junbeom-Son <junbeom247@gmail.com>
2024-09-01 10:14:56 +0900

    refactor : modify controller to rest controller

--- hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/controller/MemberController.java
+++ hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/controller/MemberController.java
@@ -1,52 +1,40 @@
 package com.junbeom.hexagonalarchitecture.controller;
 
+import com.junbeom.hexagonalarchitecture.controller.dto.MemberRequest;
 import com.junbeom.hexagonalarchitecture.domain.Address;
 import com.junbeom.hexagonalarchitecture.domain.Member;
 import com.junbeom.hexagonalarchitecture.service.MemberService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/members/new")
-    public String createForm(Model model) {
-        model.addAttribute("memberForm", new MemberForm());
-        return "members/createMemberForm";
-    }
-
     @PostMapping("/members/new")
-    public String create(@Valid MemberForm form, BindingResult result) {
+    public ResponseEntity<Long> create(@RequestBody MemberRequest memberDto) {
 
-        if (result.hasErrors()) {
-            return "members/createMemberForm";
-        }
-
-        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Address address = new Address(memberDto.city(), memberDto.street(), memberDto.zipcode());
 
         Member member = new Member();
-        member.setName(form.getName());
+        member.setName(memberDto.name());
         member.setAddress(address);
 
-        memberService.join(member);
-        return "redirect:/";
+        Long memberId = memberService.join(member);
+        return new ResponseEntity<>(memberId, HttpStatus.CREATED);
     }
 
-    @GetMapping("/members")
-    public String list(Model model) {
-        List<Member> members = memberService.findMembers();
-        model.addAttribute("members", members);
-        return "members/memberList";
-    }
+//    @GetMapping("/members")
+//    public String list(Model model) {
+//        List<Member> members = memberService.findMembers();
+//        model.addAttribute("members", members);
+//        return "members/memberList";
+//    }
 
 }
