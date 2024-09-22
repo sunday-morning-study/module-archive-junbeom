44a548a34451b0ffdc409efc6eeac3786da111e3
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-09-22 12:00:18 +0900

    chore: remove unused method

--- hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/adapter/in/web/ItemController.java
+++ hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/adapter/in/web/ItemController.java
@@ -36,22 +36,6 @@ public class ItemController {
         return ResponseEntity.ok(itemService.findItemById(id));
     }
 
-    @GetMapping("items/{itemId}/edit")
-    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
-        Book item = (Book) itemService.findOne(itemId);
-
-        BookForm form = new BookForm();
-        form.setId(item.getId());
-        form.setName(item.getName());
-        form.setPrice(item.getPrice());
-        form.setStockQuantity(item.getStockQuantity());
-        form.setAuthor(item.getAuthor());
-        form.setIsbn(item.getIsbn());
-
-        model.addAttribute("form", form);
-        return "items/updateItemForm";
-    }
-
     @PostMapping("items/{itemId}/edit")
     public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
 
