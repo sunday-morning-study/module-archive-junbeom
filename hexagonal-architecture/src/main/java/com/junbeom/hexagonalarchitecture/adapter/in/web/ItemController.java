b5e117f59386460639b15ce1ef62ffffeb2838d2
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-09-22 10:56:53 +0900

    refactor: modify controller to rest controller

--- hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/adapter/in/web/ItemController.java
+++ hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/adapter/in/web/ItemController.java
@@ -5,26 +5,16 @@ import com.junbeom.hexagonalarchitecture.domain.item.Book;
 import com.junbeom.hexagonalarchitecture.domain.item.Item;
 import com.junbeom.hexagonalarchitecture.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ItemController {
 
     private final ItemService itemService;
-
-    @GetMapping("/items/new")
-    public String createForm(Model model) {
-        model.addAttribute("form", new BookForm());
-        return "items/createItemForm";
-    }
 
     @PostMapping("/items/new")
     public String create(BookForm form) {
