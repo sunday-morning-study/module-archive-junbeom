34bd716a0b209a95b824d8294d15114328020d90
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-10-27 11:13:03 +0900

    feat: create constructor using order item

--- hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/adapter/in/web/dto/OrderItemResponse.java
+++ hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/adapter/in/web/dto/OrderItemResponse.java
@@ -1,8 +1,14 @@
 package com.junbeom.hexagonalarchitecture.adapter.in.web.dto;
 
+import com.junbeom.hexagonalarchitecture.domain.OrderItem;
+
 public record OrderItemResponse(
         Long id,
         Long itemId,
         int orderPrice,
         int count
-) {}
+) {
+    public OrderItemResponse(OrderItem orderItem) {
+        this(orderItem.getId(), orderItem.getItem().getId(), orderItem.getOrderPrice(), orderItem.getCount());
+    }
+}
