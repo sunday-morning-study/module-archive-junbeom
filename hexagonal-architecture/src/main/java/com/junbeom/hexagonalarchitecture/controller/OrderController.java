a3853d824e9517ad8fa4f1ef4e55bbed00ff5b93
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2024-10-27 10:23:23 +0900

    refactor: convert controller to rest controller

--- hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/controller/OrderController.java
+++ hexagonal-architecture/src/main/java/com/junbeom/hexagonalarchitecture/controller/OrderController.java
@@ -1,59 +1,42 @@
 package com.junbeom.hexagonalarchitecture.controller;
 
-import com.junbeom.hexagonalarchitecture.domain.Member;
-import com.junbeom.hexagonalarchitecture.domain.Order;
-import com.junbeom.hexagonalarchitecture.domain.item.Item;
-import com.junbeom.hexagonalarchitecture.repository.OrderSearch;
-import com.junbeom.hexagonalarchitecture.application.ItemService;
-import com.junbeom.hexagonalarchitecture.application.MemberService;
 import com.junbeom.hexagonalarchitecture.application.OrderService;
+import com.junbeom.hexagonalarchitecture.domain.Order;
+import com.junbeom.hexagonalarchitecture.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class OrderController {
 
     private final OrderService orderService;
-    private final MemberService memberService;
-    private final ItemService itemService;
-
-    @GetMapping("/order")
-    public String createForm(Model model) {
-
-        List<Member> members = memberService.findMembers();
-        List<Item> items = itemService.findAllItems();
-
-        model.addAttribute("members", members);
-        model.addAttribute("items", items);
-
-        return "order/orderForm";
-    }
 
     @PostMapping("/order")
-    public String order(@RequestParam("memberId") Long memberId,
-                        @RequestParam("itemId") Long itemId,
-                        @RequestParam("count") int count) {
+    public ResponseEntity<Long> order(@RequestParam("memberId") Long memberId,
+                                @RequestParam("itemId") Long itemId,
+                                @RequestParam("count") int count) {
 
         orderService.order(memberId, itemId, count);
-        return "redirect:/orders";
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
     @GetMapping("/orders")
-    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+    public ResponseEntity<List<Order>> orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
         List<Order> orders = orderService.findOrders(orderSearch);
         model.addAttribute("orders", orders);
 
-        return "order/orderList";
+        return ResponseEntity.status(HttpStatus.OK).body(orders);
     }
 
     @PostMapping("/orders/{orderId}/cancel")
-    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+    public ResponseEntity<Long> cancelOrder(@PathVariable("orderId") Long orderId) {
         orderService.cancelOrder(orderId);
-        return "redirect:/orders";
+        return ResponseEntity.status(HttpStatus.OK).body(orderId);
     }
 }
